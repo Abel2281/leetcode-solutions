01125e2f046f8998886665b125968621b9ca0441
Abel S Mathew <152721736+Abel2281@users.noreply.github.com>
2026-08-15 17:36:24 +0530

    Time: 7 ms (97.20%) | Memory: 37.6 MB (22.68%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> freq1, freq2;
-        int n = nums1.size();
-        for(int i=0; i<n; i++){
-            if(find(nums2.begin(),nums2.end(),nums1[i])==nums2.end()) freq1[nums1[i]]++;
-        }
-        for(int i=0;i<nums2.size();i++){
-            if(find(nums1.begin(),nums1.end(),nums2[i])==nums1.end()) freq2[nums2[i]]++;
-        }
-        vector<vector<int>> answer;
-        vector<int> temp;
-        for(int num: nums1){
-            if(freq1[num]!=0){
-                temp.push_back(num);
-                freq1[num]=0;
-            }
-        }
-        answer.push_back(temp);
-        vector<int> temp2;
-        for(int num: nums2){
-            if(freq2[num]!=0){
-                temp2.push_back(num);
-                freq2[num]=0;
-            }
-        }
-        answer.push_back(temp2);
-        return answer;
+        unordered_set<int> set1(nums1.begin(),nums1.end());
+        unordered_set<int> set2(nums2.begin(),nums2.end());
 
+        vector<int> ans1,ans2;
+
+        for(int num: set1){
+            if(set2.find(num)==set2.end()) ans1.push_back(num);
+        }
+        for(int num: set2){
+            if(set1.find(num)==set1.end()) ans2.push_back(num);
+        }
+        return {ans1,ans2};
     }
 };
