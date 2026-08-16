39b25cd0a3f9ef47a3bf9e5547b2935c011a510e
Abel S Mathew <152721736+Abel2281@users.noreply.github.com>
2026-08-16 17:19:58 +0530

    Time: 4 ms (14.18%) | Memory: 22 MB (60.42%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -2,31 +2,27 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int> st;
-        for(int n:asteroids){
-            if(n>0){
+         for (int n : asteroids) {
+            bool destroyed = false;
+            while (!st.empty() && st.top() > 0 && n < 0) {
+                if (st.top() + n == 0) {
+                    st.pop();
+                    destroyed = true;
+                    break;
+                }
+                else if (st.top() + n < 0) {
+                    st.pop();
+                }
+                else {
+                    destroyed = true;
+                    break;
+                }
+            }
+            if (!destroyed) {
                 st.push(n);
-            }else{
-                if(st.empty()) st.push(n);
-                else if(!st.empty() && st.top()<0){
-                    st.push(n);
-                    continue;
-                }
-                while(!st.empty() && st.top()>0){
-                    if(st.top()+n==0){
-                        st.pop();
-                        break;
-                    }
-                    else if(st.top()+n<0){
-                        st.pop();
-                        if(st.empty() || st.top()<0) st.push(n);
-                    }
-                    else{
-                        break;
-                    }   
-                }
-
             }
         }
+
         vector<int> res;
         while(!st.empty()){
             res.push_back(st.top());
